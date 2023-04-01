88c7b8bac597a92e3682061ea53c333aea056150
Anis Shili <Anis.Shili@ibm.com>
2023-04-01 06:16:40 -0400

    Added QR code generator

---
+++ qrcode.js
@@ -0,0 +1,11 @@
+const qr = require('qrcode');
+
+const url = 'https://visionary-twilight-b43b45.netlify.app/out.pdf'
+
+qr.toDataURL(url, function (err, data) {
+  if (err) {
+    console.log('Error generating QR code: ', err);
+    return;
+  }
+  console.log('<img src="' + data + '">');
+});
